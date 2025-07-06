8277ee3ce644eacb62456021006f2f526de5d93c
frannSDR <frannm1703@gmail.com>
2025-07-06 19:47:05 -0300

    metodos de get y add

---
+++ src/controller.js
@@ -0,0 +1,15 @@
+import { pool } from './database.js';
+
+class BibliotecaController {
+
+    async getAll(req, res) {
+        const [result] = await pool.query('SELECT * FROM Libros');
+        res.json(result);
+    }
+
+    async add(req,res) {
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año-publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro,nombre], [libro.autor], [libro.categoria], [libro.año-publicacion], [libro.ISBN])
+        res.json({"Id insertado": result.insertId})
+    }
+}
