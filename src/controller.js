268336df262f12719dec36cbfe862764befbcbb7
frannSDR <frannm1703@gmail.com>
2025-07-06 20:27:32 -0300

    metodo delete

--- src/controller.js
+++ src/controller.js
@@ -12,6 +12,12 @@ class BibliotecaController {
         const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN])
         res.json({"Id insertado": result.insertId})
     }
+
+    async delete(req, res) {
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`, [libro.id])
+        res.json({"Registros eliminados": result.affectedRows});
+    }
 }
 
 export const libro = new BibliotecaController();
